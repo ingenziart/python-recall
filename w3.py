cc62acf7a651ed58152dcee02fe2228fed56b62b
ingenziart <104985332+ingenziart@users.noreply.github.com>
2024-11-01 11:51:07 -0400

    list access

--- w3.py
+++ w3.py
@@ -11,3 +11,16 @@ print(c)
 age = 36
 tx = f"mynamje is aime and i am {age}"
 print(tx)
+
+num = 34
+str1 = f"my rate{num}"
+print(str1)
+
+# escaping character \"\"
+text3 = 'halland is from "viking" clan '
+print(text3)
+
+# accs the list
+
+thislist = ["mango", "banana", "passion", "blueberry", "stwaberry"]
+print(thislist[-4:-1])
