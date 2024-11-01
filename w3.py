f57b0a680dc31641d338a12fb630722ebfd2de6b
ingenziart <104985332+ingenziart@users.noreply.github.com>
2024-11-01 10:47:50 -0400

    concatenate str with int

---
+++ w3.py
@@ -0,0 +1,13 @@
+x = "hello word "
+print(x.replace("h", "j"))
+
+# concatenate
+
+a = "hello"
+b = "januzaj"
+c = a + " " + b
+print(c)
+# concatenate str with int ypu add f in the beginning and {}.
+age = 36
+tx = f"mynamje is aime and i am {age}"
+print(tx)
