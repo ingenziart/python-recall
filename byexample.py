bb7ac2f51d509d15b1c49278635021c3222cd91d
ingenziart <104985332+ingenziart@users.noreply.github.com>
2024-10-31 22:38:59 -0400

    python by example recall

---
+++ byexample.py
@@ -0,0 +1,14 @@
+text = input("inter text vslue: ")
+
+num1 = int(input("enter the number: "))
+num2 = int(input("enter the second value:"))
+answer = num1 + num2
+print(answer)
+
+num1 = 12
+if num1 > 10:
+    print("its over")
+elif num1 == 10:
+    print("this is equal")
+else:
+    print("none of them")
---
+++ oop.py
@@ -0,0 +1,11 @@
+print(type("hello word"))
+
+x = 1
+print(type(x))
+
+
+def hello():
+    print("hello")
+
+
+print(type(hello))
